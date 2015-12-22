fc66580265d16a7ad609cea4cc859b45aa7b9c80
Allison Yee <alcyee@berkeley.edu>
2015-12-21 22:14:55 -0800

    live search, organized and commented js

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,49 +1,57 @@
-// Lightbox 
-var photo_index = 0;
+// get photos from Flickr
+var get_photos = function(q) {
+  var q_url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=94907644bb851ac1e29e11bb0a5f7751&text=" + q + "&sort=relevance&format=json&nojsoncallback=1";
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", q_url, false);
+  xhr.send();
+  var response = JSON.parse(xhr.response);
+  return response["photos"]["photo"].slice(0,8);
+}
 
-var domReady = function(callback) {
-  document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
-};
+// generate image URL from Flickr data
+var url_maker = function(photo) {
+  return "https://farm" + photo['farm'] + ".staticflickr.com/" + photo['server'] + "/" + photo['id'] + "_" + photo['secret'] + "_b.jpg";
+}
 
-var update_content = function(index) {
-  photo_index = index;
+// update gallery with search results
+var update_gallery = function(photos) {
   var squares = document.getElementsByClassName('square');
-  var image_href = squares[index].href;
-  document.getElementById('content').innerHTML = '<img src="' + image_href + '" /><div class="title">' + photos[index]['title'] + '</div>';
-};
-
-domReady(function() {
-  var squares = document.getElementsByClassName('square');
-
-  // update gallery with photos
   for (var i = 0; i < photos.length; i++) {
     var url = "https://farm" + photos[i]['farm'] + ".staticflickr.com/" + photos[i]['server'] + "/" + photos[i]['id'] + "_" + photos[i]['secret'] + "_b.jpg";
     squares[i].href = url;
     squares[i].style.backgroundImage = 'url(' + url + ')';
   }
+}
 
-  var triggers = document.getElementsByClassName('lightbox_trigger');
-  var lightbox = document.getElementById('lightbox');
-  for (var i = 0; i < triggers.length; i++) {
-    (function(index) {
-      triggers[i].onclick = function(e) {
-        console.log(index);
-        e.preventDefault();
-        update_content(index);
-        lightbox.style.display = 'block';
+// populate lightbox with correct image and title
+var update_content = function(photos, index) {
+  if (index < 0 || index > 7) return;
+  curr_photo = index;
+  var squares = document.getElementsByClassName('square');
+  var image_href = squares[index].href;
+  document.getElementById('content').innerHTML = '<img src="' + image_href + '" /><div class="title">' + photos[index]['title'] + '</div>';
+};
 
-        document.getElementById('btn_prev').onclick = function(e) {
-          e.preventDefault();
-          update_content(photo_index-1);
-        }
-        document.getElementById('btn_next').onclick = function(e) {
-          e.preventDefault();
-          update_content(photo_index+1);
-        }
-      }
-    })(i);
+// update preview image and title in lightbox navigation
+var update_previews = function(photos) {
+  if (curr_photo == 0) {
+    // make prev of first photo point to the last photo
+    document.getElementById('prev_preview').style.backgroundImage = 'url(' + url_maker(photos[7]) + ')';
+    document.getElementById('prev_title').innerHTML = photos[7]['title'];
+    document.getElementById('next_preview').style.backgroundImage = 'url(' + url_maker(photos[1]) + ')';
+    document.getElementById('next_title').innerHTML = photos[1]['title'];
   }
-  // lightbox.onclick = function() {
-  //   lightbox.style.display = 'none';
-  // }
-});
+  else if (curr_photo == 7) {
+    // make next of last photo point to first photo
+    document.getElementById('prev_preview').style.backgroundImage = 'url(' + url_maker(photos[6]) + ')';
+    document.getElementById('prev_title').innerHTML = photos[6]['title'];
+    document.getElementById('next_preview').style.backgroundImage = 'url(' + url_maker(photos[0]) + ')';
+    document.getElementById('next_title').innerHTML = photos[0]['title'];
+  }
+  else {
+    document.getElementById('prev_preview').style.backgroundImage = 'url(' + url_maker(photos[curr_photo-1]) + ')';
+    document.getElementById('prev_title').innerHTML = photos[curr_photo-1]['title'];
+    document.getElementById('next_preview').style.backgroundImage = 'url(' + url_maker(photos[curr_photo+1]) + ')';
+    document.getElementById('next_title').innerHTML = photos[curr_photo+1]['title'];
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,8 +1,76 @@
-// Flickr API
+// global var to keep track of current photo in lightbox 
+var curr_photo = 0;
+// global var to keep track of current set of photos
+var photos;
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4ec1d581108a2f696387b66aa9664700&tags=san+francisco&sort=interestingness-desc&format=json&nojsoncallback=1&api_sig=c0654978ac9b05e4c49d3251d09f7aaf", false);
-xhr.send();
+var domReady = function(callback) {
+  document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
+};
 
-var response = JSON.parse(xhr.response);
-var photos = response["photos"]["photo"].slice(0,8);
+domReady(function() {
+  // populate gallery initially
+  photos = get_photos("san+francisco");
+  update_gallery(photos);
+
+  var search = document.getElementById('search');
+  var timer;
+  search.onkeyup = function() {
+    clearTimeout(timer);
+    timer = setTimeout(function() {
+      var query = search.value.split(' ').join('+');
+      photos = get_photos(query);
+      update_gallery(photos);
+    }, 1000)
+  }
+
+  var lightbox = document.getElementById('lightbox');
+  var triggers = document.getElementsByClassName('lightbox_trigger');
+
+  // add keydown functionality
+  document.onkeydown = function(e) {
+    if (lightbox.style.display == 'block') {
+      // right arrow
+      if (e.keyCode == 39) {
+        if (curr_photo == 7) update_content(photos, 0);
+        else update_content(photos, curr_photo+1);
+      }
+      // left arrow
+      else if (e.keyCode == 37) {
+        if (curr_photo == 0) update_content(photos, 7);
+        else update_content(photos, curr_photo-1);
+      }
+      // esc 
+      else if (e.keyCode == 27) {
+        lightbox.style.display = 'none';
+      }
+    }
+  };
+
+  // open lightbox with correct image and set prev/next buttons
+  for (var i = 0; i < triggers.length; i++) {
+    (function(index) {
+      triggers[i].onclick = function(e) {
+        e.preventDefault();
+        update_content(photos, index);
+        update_previews(photos);
+        lightbox.style.display = 'block';
+
+        document.getElementById('btn_prev').onclick = function(e) {
+          e.preventDefault();
+          if (curr_photo == 0) update_content(photos, 7);
+          else update_content(photos, curr_photo-1);
+          update_previews(photos);
+        }
+        document.getElementById('btn_next').onclick = function(e) {
+          e.preventDefault();
+          if (curr_photo == 7) update_content(photos, 0);
+          else update_content(photos, curr_photo+1);
+          update_previews(photos);
+        }
+      }
+    })(i);
+  }
+  document.getElementById('content').onclick = function() {
+    lightbox.style.display = 'none';
+  }
+});
